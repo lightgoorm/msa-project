a9f4fb1dc9b9d3b60e77856f9b770d7009516278
함운경 <woonkyoung_ham1@kolon.com>
2025-02-04 20:58:46 +0900

    second commit

---
+++ src/main/kotlin/com/example/msaproject/MsaProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.example.msaproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MsaProjectApplication
+
+fun main(args: Array<String>) {
+    runApplication<MsaProjectApplication>(*args)
+}
